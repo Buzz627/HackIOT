24e038f84d75953ce9b37d2ec5bb1d8772a9fa79
Buzz <Buzz627@gmail.com>
2017-12-09 16:16:17 -0500

    added knn

--- hack.py
+++ hack.py
@@ -1,5 +1,28 @@
 import requests
 import json
+import time
+
+def distance(point1, point2):
+	total=0
+	for key in point1.keys():
+		if key=="classification":
+			pass
+		try:
+			total+=abs(point1[key]-point2[key])
+		except: return float('inf')
+	return total
+
+def findKNearest(k, datapoint, dataset):
+	bestnums=[float('inf')]*k
+	bestPoint=dataset.keys()[0]
+	for d in dataset:
+		current=distance(datapoint, dataset[d])
+		if current<best:
+			best=current
+			bestPoint=dataset[d]
+	return 
+
+
 
 url = "https://api.microshare.io/share/this.is.buzz.decoded"
 
@@ -7,28 +30,46 @@ querystring = {"details":"true","page":"","perPage":""}
 
 headers = {
     'authorization': "Bearer 5e8987b0475ef7b5e433ea360a7daf59d108a37d2338f72f340a18c70a8c74cc",
-    'cache-control': "no-cache",
-    'postman-token': "b8c29d8a-9fd9-9769-3185-21c8982c43bd"
+    # 'cache-control': "no-cache",
+    # 'postman-token': "00156731-b784-8fe5-75f3-012afde3cf35"
     }
 
 response = requests.request("GET", url, headers=headers, params=querystring)
+# print(json.dumps(json.loads(response.text), indent=2))
 
 data={}
 for point in json.loads(response.text)["objs"]:
-	dataType=point["data"]["type"]
-	timeStamp=point['createDate']["$date"]
-	measurement=point["data"]["measure"]
-	measureStr=point["data"]["value"]
-	# print dataType
-	# for i in point["data"]:
-	# 	print i, point["data"][i]
-	# print ""
-	# print point['createDate']["$date"]
-	if dataType not in data:
-		data[dataType]=[]
-	data[dataType].append({"timeStamp":timeStamp, "dataType":dataType, 'measurement':measurement, 'measureStr':measureStr})
-print json.dumps(data, indent=2)
-print data.keys()
+	try:
+		# print point["id"]
+		dataType=point["data"]["type"]
+		timeStamp=point['createDate']["$date"]/1000
+		measurement=point["data"]["measure"]
+		measureStr=point["data"]["value"]
+		# print dataType
+		# for i in point["data"]:
+		# 	print i, point["data"][i]
+		# print ""
+		# print point['createDate']["$date"]
+		if str(timeStamp) not in data:
+			data[str(timeStamp)]={}
+		data[str(timeStamp)][dataType]=measurement
+		data[str(timeStamp)]['classification']=random
+		# print json.dumps(data, indent=2)
+		# print data.keys()
 
 
-# print(json.dumps(json.loads(response.text), indent=2))
+		# print(json.dumps(json.loads(response.text), indent=2))
+
+	except:
+		pass
+
+
+
+for i in data.keys():
+	print time.ctime(float(i)), data[str(i)] 
+	# for j in data[i]:
+	# 	if j["timeStamp"]>m:
+	# 		m=j["timeStamp"]
+	# print time.ctime(m)
+
+
